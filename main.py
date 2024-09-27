4ff7e0b2f2da993fd48566478063a1e203d88261
kayikc <kayikchoi20@gmail.com>
2024-09-28 01:03:45 +1000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.title("🌠 Meteorite Landings Dashboard")
 
 @st.cache_data
 def load_data():
-    df = pl.read_csv("E:\streamlit_project\.meteorite_landings\data\Meteorite_Landings_20240927.csv")
+    df = pl.read_csv("Meteorite_Landings_20240927.csv")
     return df
 
 def rename_columns(df):
